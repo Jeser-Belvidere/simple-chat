2d948d14ef800eb632450ce86bfeec82bcadb8aa
Ivan Petrovskiy <jelbelvidere@gmail.com>
2023-02-19 19:37:06 +0300

    Added global guard, cors init config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,15 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { AuthGuard } from '@/common/guards/jwt.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //TODO: edite cors config for dev/production mode
+  app.enableCors({
+    origin: '*',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +18,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  app.useGlobalGuards(new AuthGuard());
+
   await app.listen(parseInt(process.env.APP_PORT || '3000', 10), () => {
     console.log(`APP HAS BEEN STARTED ON PORT ${process.env.APP_PORT}`);
   });
