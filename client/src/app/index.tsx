9d30e0fcafcbc84403d0457f1027f5bffa10150a
Ivan Petrovskiy <jelbelvidere@gmail.com>
2023-03-15 18:16:33 +0300

    Added router instance to app

--- client/src/app/index.tsx
+++ client/src/app/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import Router from './router'
 import './index.css'
+import '@shared/stores'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={Router} />
+		<RouterProvider router={Router} />,
 	</React.StrictMode>,
 )
